c4af49fa5af578b9374c47c66077d0d13221c289
Tarun S <tars@akamai.com>
2024-07-18 15:16:10 +0530

    Minor changes

--- plot.py
+++ plot.py
@@ -2,46 +2,41 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
-df1 = pd.read_csv("./gzip_c6.csv")
-df2 = pd.read_csv("./gzip_c7.csv")
+df1 = pd.read_csv("/path/to/csv") #Path A
+df2 = pd.read_csv("/path/to/csv") #Path B
 
-l1 = 6
-l2 = 7
-version = '1.2.11'
-is_for = 'time'
+l1 = 6 #Level A
+l2 = 7 #Level B
+version = '1.3.1' #Version
+is_for = 'time' #Parameter: time/size
 
-files = df1["file"].to_list()
+files = df2["Original File Name"].to_list()
 
 if is_for == 'time':
   mode = 'Time Taken'
   unit = 'ms'
-  size1 = df1["deflate_compression_time(ms)"].to_list() 
-  size2 = df2["deflate_compression_time(ms)"].to_list() 
+  value1 = df1["Time Taken in Compression(ms)"].to_list() 
+  value2 = df2["Time Taken in Compression(ms)"].to_list() 
 else:
   mode = 'Compressed Size'
   unit = 'kB'
-  size1 = df1["compressed_size(bytes)"].to_list() 
-  size2 = df2["compressed_size(bytes)"].to_list() 
+  value1 = df1["Compressed File Size(B)"].to_list() 
+  value2 = df2["Compressed File Size(B)"].to_list() 
 
-rss1 = df1["resident_set_size(bytes)"].to_list()
-rss2 = df2["resident_set_size(bytes)"].to_list()
-
-rss_change = []
 diff = []
 
 if is_for == 'size':
-  for i in range(len(size1)):
-      size1[i] = size1[i] / 1024.0
+  for i in range(len(value1)):
+      value1[i] = value1[i] / 1024.0
 
-  for i in range(len(size2)):
-      size2[i] = size2[i] / 1024.0
+  for i in range(len(value2)):
+      value2[i] = value2[i] / 1024.0
 
-for i in range(len(size1)):
-  if size1[i] == 0:
+for i in range(len(value1)):
+  if value1[i] == 0:
     diff.append(0)
   else:
-    diff.append(round((((size1[i] - size2[i]) / size1[i])*100), 2))
-  rss_change.append(rss2[i] - rss1[i])
+    diff.append(round((((value1[i] - value2[i]) / value1[i])*100), 2))
 
 net_change = sum(diff) / len(diff)
 
@@ -56,8 +51,8 @@ fig, ax = plt.subplots(figsize=(20,10))
 width = 0.35
 x = range(len(files))
 
-rects1 = ax.bar(x, size1, width, label= f'{mode} at Level {l1}')
-rects2 = ax.bar([i+width for i in x], size2, width, label = f'{mode} at Level {l2}')
+rects1 = ax.bar(x, value1, width, label= f'{mode} at Level {l1}') 
+rects2 = ax.bar([i+width for i in x], value2, width, label = f'{mode} at Level {l2}') 
 rects3 = ax.bar([i+width*2 for i in x], diff, width, label = f'Percentage change in {is_for}', color = 'lightgray') 
 
 ax.set_xlabel('File Name')
@@ -71,7 +66,11 @@ ax.legend()
 def add_labels(bars):
   for bar in bars:
     height = bar.get_height()
-    ax.annotate(f'{height: .2f}', xy = (bar.get_x() + bar.get_width() / 2, height), xytext = (0,3), textcoords = "offset points", ha = "center", va = 'bottom', fontsize=8, rotation = 45)
+    if is_for == 'time':
+      height = int(height) 
+    else:
+      height = round(height, 2) 
+    ax.annotate(f'{height}', xy = (bar.get_x() + bar.get_width() / 2, height), xytext = (0,3), textcoords = "offset points", ha = "center", va = 'bottom', fontsize=8, rotation = 45)
 
 add_labels(rects1)
 add_labels(rects2)
